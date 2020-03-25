e36a9a0b0bc25495c70aca7b8ecccf738f6e57fe
lwzearfoss <lwzearfoss@gmail.com>
2020-03-24 17:19:54 -0700

    Create prod.

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(kv95)z%&(txky(%r-ces!@qbv$75%s1lt5hx4%myj-_denoj@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/login/"
